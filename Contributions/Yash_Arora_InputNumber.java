aab3d4688f14cb8d0e9bc8b6d808fff1c4274155
yashar1908 <73465642+yashar1908@users.noreply.github.com>
2020-10-28 04:19:11 +0530

    removed sum java and added a program

---
+++ Contributions/Yash_Arora_InputNumber.java
@@ -0,0 +1,15 @@
+/*
+Problem: Write a java program to find the sum of digits of a given number.
+*/
+
+import java.util.*;
+public class digitSum
+{
+    public void main()
+    {
+        Scanner s = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int n = s.nextInt();
+		System.out.println(n); //output the number that was taken as input from the user
+	}
+}
