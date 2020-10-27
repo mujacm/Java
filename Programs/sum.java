9dbb67a261ea4130468af9ffde8be4dbf2a954b9
shivanshdharsingh <71934690+shivanshdharsingh@users.noreply.github.com>
2020-10-28 02:19:27 +0530

    Updated sum.java and added a image

--- Programs/sum.java
+++ Programs/sum.java
@@ -7,17 +7,9 @@ public class digitSum
 {
     public void main()
     {
-        Scanner s = new Scanner(System.in);
-        int n,t,sum=0;
-        System.out.print("Enter a number : ");
-        n=s.nextInt();
-        s.nextLine();
-        t=n;
-        while(t!=0)
-        {
-            sum+= (t/10);
-            t/=10;
-        }
-        System.out.println("Sum of digits of "+n+" is = "+sum);
+       sum(30,40);
     }
+void sum(int s , int n){
+    System.out.println(s+n);
+}
 }
