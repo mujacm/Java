b70cdc1835306072ef6961acba4150af5e385f69
yashar1908 <73465642+yashar1908@users.noreply.github.com>
2020-10-28 03:13:03 +0530

    updated program

---
+++ Contributions/Yash Difference.java
@@ -0,0 +1,17 @@
+/*
+Problem: Write a java program to find the sum of digits of a given number.
+*/
+
+import java.util.*;
+public class digitSum
+{
+    public void main()
+    {
+      System.out.println("Difference of the numbers is: ");
+	  diff(5,3);
+    }
+	void diff(int a, int b)
+	{
+		System.out.println(a-b);
+	}
+}
