07358bfcf90aaa7d64229099adc6ef9dc537458d
shivanshdharsingh <71934690+shivanshdharsingh@users.noreply.github.com>
2020-10-28 02:46:26 +0530

    Added sums file to contributions

---
+++ Contributions/Shivansh_sum.java
@@ -0,0 +1,15 @@
+/*
+Problem: Write a java program to find the sum of digits of a given number.
+*/
+
+import java.util.*;
+public class digitSum
+{
+    public void main()
+    {
+       sum(30,40);
+    }
+void sum(int s , int n){
+    System.out.println(s+n);
+}
+}
