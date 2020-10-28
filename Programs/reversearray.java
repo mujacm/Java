fd8cdd98419a9f2875e3dff538552cac4e5246b5
sarthak-dv <73549869+sarthak-dv@users.noreply.github.com>
2020-10-28 09:50:44 +0530

    Updated reverse array

--- Programs/reversearray.java
+++ Programs/reversearray.java
@@ -7,34 +7,15 @@ public class repo
 {
     public static void main(String [] args)
     {
-        Scanner s= new Scanner(System.in);
-        System.out.println("Enter number of elements: ");
-        int n=s.nextInt(); s.nextLine();
-        System.out.println("\nEnter "+n+" integers for the array: ");
-        int[] arr = new int[n];
-        int i=0,j=n-1;
-        for(i=0;i<n;i++)
+        for(int i=0;i<args.length;i++)
         {
-            arr[i]=s.nextInt();
-        }
-        System.out.println("\nThe elements entered are:");
-        for(i=0;i<n;i++)
-        {
-            System.out.print(arr[i]+" ");
-        }
-        for(i=0;i<n;)
-        {
-            arr[i]=arr[i]+arr[j];
-            arr[j]=arr[i]-arr[j];
-            arr[i]=arr[i]-arr[j];
-            i++;
-            j--;
-        }
-        System.out.println("\nThe elements after reversing are:");
-        for(i=0;i<n;i++)
-        {
-            System.out.print(arr[i]+" ");
-        }
+            String s="";
+            for(int j=s.length()-1;j>=0;j--)
+            {
+                s=s+args[i].charAt(j);
+            }
+            System.out.print(s+" ");
     }
+}
 }
 
